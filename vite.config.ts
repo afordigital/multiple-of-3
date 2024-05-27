1e36320e779fffcc224fa2605e95b8d666db93af
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-05-27 21:40:35 +0200

    feat: insertTypesEntry added

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: "src/main.ts",
